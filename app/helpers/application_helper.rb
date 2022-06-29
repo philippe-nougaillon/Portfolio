8258488be33b374b598ebad88a05a54b9b25f34a
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-06-29 10:50:53 +0200

    homepage / navbar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def navbar_item(controller_name, path, label = nil)
+    is_active = params[:controller] == controller_name
+
+    render(inline: %{
+        <li>
+          <%= link_to '#{ url_for(path) }', 
+                      class: 'flex gap-4btn #{is_active ? "btn-outline btn-primary text-white" : "btn-ghost" }' do %>
+              <span class="flex-1">#{ label ? label : controller_name.humanize }</span>
+          <% end %>
+        </li>
+    })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :projets
 
-  root 'projets#index'
+  get 'pages/home'
+
+  root 'pages#home'
 end
