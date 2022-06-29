3f778e1886b4f0f49d0f93eef59a7ee9002f2deb
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-06-29 11:25:15 +0200

    stimulus-notification added

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.0.0/dist/stimulus-notification.es.js"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.9.4/dist/hotkeys.esm.js"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.50.0/dist/index.js"
