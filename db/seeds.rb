a1bd16453a00fe4ede5a7cf18aa195d544becea9
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2022-07-04 15:40:40 +0200

    Design tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Projet.create!([
-  {nom: "AEMMS", url: "https://aemms-v3.herokuapp.com/", contenu: "Système de management de la qualité, conforme à la Norme européenne EN 14730-2, pour les entreprises de soudage par aluminothermie.", poids: 100, tag_list: nil},
-  {nom: "RNMA", url: "http://rnma.fr/", contenu: "Les Maisons des associations au service de la vie associative locale", poids: 90, tag_list: nil},
+  {nom: "AEMMS", url: "https://aemms-v3.herokuapp.com/", contenu: "Système de management de la qualité pour les entreprises de soudage par aluminothermie.", poids: 100, tag_list: nil},
+  {nom: "RNMA", url: "https//www.rnma.fr/", contenu: "Les Maisons des associations au service de la vie associative locale.", poids: 90, tag_list: nil},
+  {nom: "INNOSERV", url: "https://innoserv-project.herokuapp.com/", contenu: "INNOSERV est un projet développant un programme de recherche à l'échelle de l'UE pour évaluer l'innovation dans le domaine des services sociaux (en particulier pour les services de santé, d'éducation et d'aide sociale). Cela aidera à définir les futures activités de recherche.", poids: 85, tag_list: nil},
   {nom: "IAE PLANNING", url: "https://planning-iae.herokuapp.com/", contenu: "Application web de planification de cours. Collaborative, flexible et simple d'utilisation", poids: 80, tag_list: nil},
-  {nom: "Disturbances", url: "https://disturbances.herokuapp.com/", contenu: "", poids: 4, tag_list: nil},
+  {nom: "Disturbances", url: "https://disturbances.herokuapp.com/", contenu: "Outil de collecte et d'analyse des perturbations SNCF-TER Grand Est", poids: 70, tag_list: nil},
+  {nom: "LTSF pro", url: "https://www.ltsf.pro", contenu: "Le T-shirt français, en B2B, à l'usage des grossistes", poids: 60, tag_list: nil},
+  {nom: "AEMMS-MOBILE", url: "https://aemms-v3.herokuapp.com/about", contenu: "Nouvelle application mobile pour les opérations de terrain", poids: 50, tag_list: nil},
   {nom: "Prestofacto", url: "https://prestofacto.philnoug.com/", contenu: "Application libre de facturation de services journaliers", poids: 2, tag_list: nil},
-  {nom: "AEMMS-MOBILE", url: "", contenu: "", poids: 2, tag_list: nil},
-  {nom: "INNOSERV", url: "https://innoserv-project.herokuapp.com/", contenu: "INNOSERV est un projet développant un programme de recherche à l'échelle de l'UE pour évaluer l'innovation dans le domaine des services sociaux (en particulier pour les services de santé, d'éducation et d'aide sociale). Cela aidera à définir les futures activités de recherche.", poids: 1, tag_list: nil},
-  {nom: "Portfolio", url: "http://127.0.0.1:3000/", contenu: "", poids: -1, tag_list: nil},
-  {nom: "LTSF pro", url: "https://www.ltsf.pro", contenu: "L'éthique et la qualité d'un T-shirt conçu et fabriqué en France, à prix juste.", poids: -10, tag_list: nil}
+  {nom: "Portfolio", url: "http://127.0.0.1:3000/", contenu: "", poids: -1, tag_list: nil}
 ])
