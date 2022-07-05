28f5952f5bd7a75262aeeee640dbd65e68adc8f2
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2022-07-05 19:57:54 +0200

    Améliorations cosmétiques

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_layout_variables
 
   def set_layout_variables
-    @sitename = "Philnoug & Partners Portfolio"
+    @sitename = "Philnoug & Partners"
     @sitename_short = "P/&/P Portfolio"
     @themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"]
   end
