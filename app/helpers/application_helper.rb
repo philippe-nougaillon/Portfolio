5d328d9a74fda1b18caf102600f86a6be82afc6e
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-07-12 15:50:58 +0200

    small design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     render(inline: %{
         <li>
           <%= link_to '#{ url_for(path) }', 
-                      class: 'flex gap-4 #{ "underline" if is_active }' do %>
+                      class: 'flex gap-4 #{ "underline underline-offset-8" if is_active }' do %>
               <span class="flex-1">#{ label ? label : controller_name.humanize }</span>
           <% end %>
         </li>
