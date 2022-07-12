22558324c7857696c1e19292ff16b014991e278b
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-07-12 13:52:40 +0200

    design improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
-  def navbar_item(controller_name, path, label = nil)
-    is_active = params[:controller] == controller_name
+  def navbar_item(action_name, path, label = nil)
+
+    is_active = params[:action] == action_name
 
     render(inline: %{
         <li>
           <%= link_to '#{ url_for(path) }', 
-                      class: 'flex gap-4btn #{is_active ? "btn-outline btn-primary text-white" : "btn-ghost" }' do %>
+                      class: 'flex gap-4 #{ "underline" if is_active }' do %>
               <span class="flex-1">#{ label ? label : controller_name.humanize }</span>
           <% end %>
         </li>
