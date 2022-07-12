0dc361226fd69155a69c740abf8a7ee2a8433931
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-07-12 10:05:50 +0200

    responsive / Projet's stats

---
+++ db/migrate/20220711100922_add_commit_deploy_coffee_to_projet.rb
@@ -0,0 +1,7 @@
+class AddCommitDeployCoffeeToProjet < ActiveRecord::Migration[7.0]
+  def change
+    add_column :projets, :commit, :integer, default: 0
+    add_column :projets, :deploy, :integer, default: 0
+    add_column :projets, :coffee, :integer, default: 0
+  end
+end
