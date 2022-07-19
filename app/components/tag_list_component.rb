19eb2ed44475ca33ab58d7f5e8c443301d450d2b
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2022-07-19 11:23:27 +0200

    tag_list component / tags added to Client

---
+++ app/components/tag_list_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class TagListComponent < ViewComponent::Base
+  def initialize(tags:)
+    @tags = tags
+  end
+
+end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -65,6 +65,6 @@ class ClientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def client_params
-      params.require(:client).permit(:name, :description)
+      params.require(:client).permit(:name, :description, :tag_list)
     end
 end
