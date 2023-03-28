df9fa3f4af27b973f98b283c63364d73d39b0748
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-03-28 16:43:06 +0200

    stats animated numbers

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,16 @@
 import { Application } from "@hotwired/stimulus"
 import Notification from 'stimulus-notification'
+import AnimatedNumber from 'stimulus-animated-number'
+
 
 const application = Application.start()
 application.register('notification', Notification)
+application.register('animated-number', AnimatedNumber)
+
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
+
