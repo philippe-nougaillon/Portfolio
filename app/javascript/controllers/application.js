ec85dbe248ecfdff43863bbbbe2136f00d275583
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-09-28 14:20:15 +0200

    carousel paginate

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,10 +2,10 @@ import { Application } from "@hotwired/stimulus"
 import Notification from 'stimulus-notification'
 import AnimatedNumber from 'stimulus-animated-number'
 
-
 const application = Application.start()
 application.register('notification', Notification)
 application.register('animated-number', AnimatedNumber)
+
 
 
 // Configure Stimulus development experience
---
+++ app/javascript/controllers/carousel_paginate_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    // console.log("Hello from carousel controller")
+  }
+
+  activate(event) {
+    this.deactivate_all_buttons();
+    event.target.classList.remove("btn-outline")
+  }
+
+  deactivate_all_buttons() {
+    let elements = document.getElementsByClassName("carousel-page")
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.add('btn-outline');
+    }
+  }
+}
