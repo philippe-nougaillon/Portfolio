99088ff9a159908f258fa9c705e4e536f1903405
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-09-28 17:07:52 +0200

    carousel button designed

--- app/javascript/controllers/carousel_paginate_controller.js
+++ app/javascript/controllers/carousel_paginate_controller.js
@@ -8,13 +8,15 @@ export default class extends Controller {
 
   activate(event) {
     this.deactivate_all_buttons();
-    event.target.classList.remove("btn-outline")
+    event.target.classList.remove('btn-outline','btn-primary')
+    event.target.classList.add("btn-accent")
   }
 
   deactivate_all_buttons() {
     let elements = document.getElementsByClassName("carousel-page")
     for (var i = 0; i < elements.length; i++) {
-      elements[i].classList.add('btn-outline');
+      elements[i].classList.add('btn-outline', 'btn-primary');
+      elements[i].classList.remove('btn-accent');
     }
   }
 }
