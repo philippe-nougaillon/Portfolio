f6f9d4da03a32f2d129156634ccf38aadb6d3dd6
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-11-21 11:49:07 +0100

    Enterprise name changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :set_layout_variables
 
   def set_layout_variables
-    @sitename = "Studio 'Philnoug & Partners'"
+    @sitename = "Philnoug Consulting"
   end
 end
